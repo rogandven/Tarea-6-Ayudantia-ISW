a4dd67b538f0809005293b0e0178ecf25537075f
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-09-30 02:59:46 -0300

    Another attempt

--- src/config/configDb.js
+++ src/config/configDb.js
@@ -16,7 +16,7 @@ export const AppDataSource = new DataSource({
   username: `${DB_USERNAME}`,
   password: `${DB_PASSWORD}`,
   database: `${DATABASE}`,
-  entities: ['dist/**/*.entity.js'],
+  entities: ['*.entity.js'],
   synchronize: true, 
   logging: false,
   autoLoadEntities: true,
