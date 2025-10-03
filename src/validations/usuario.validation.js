6c28431700f72da04397a06688116c05b46ec9b5
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-10-03 12:20:17 -0300

    Ran NPM install

---
+++ src/validations/usuario.validation.js
@@ -0,0 +1,14 @@
+"use strict";
+import Joi from "joi";
+
+export const usuarioBodyValidation = Joi.object({
+    id: Joi.string().
+    email:
+    password:
+    created_at:
+    updated_at: 
+});
+
+export const usuarioQueryValidation = Joi.object({
+
+});
