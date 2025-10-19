331c2aeae875ff598b7c1aba3b71bd1d66e75aca
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-10-19 13:18:55 -0300

    Temporarily removed useAuth hook

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,16 +1,34 @@
-import { Navigate } from 'react-router-dom';
+/* import { Navigate } from 'react-router-dom';
 import { useAuth } from '@context/AuthContext';
+import Loading from '../pages/Loading';
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
 
   if (user === undefined) {
-    return window.location.reload();
+    return <Loading destination={window.location.href}></Loading>
   } else if (user === null) {
     return <Navigate to="/auth" replace />;
   }
 
   return children;
+};
+
+export default ProtectedRoute;
+*/
+
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const storedUser = JSON.parse(sessionStorage.getItem('usuario'));
+
+    const isAuthenticated = !!storedUser;
+
+    if (!isAuthenticated) {
+        return <Navigate to="/auth" />;
+    }
+
+    return children;
 };
 
 export default ProtectedRoute;
